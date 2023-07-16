82a6bb5d2cf28cdc9ea965398e90ec67e4a23601
陳國翔 <chen199940@gmail.com>
2023-07-16 16:52:56 +0800

    new

---
+++ 107/A.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<sstream>
+using namespace std;
+#define fo(b) for(int i=0;i<b;i++)
+int main(){
+    int n;
+    string data;
+    string s;
+    cin>>n;
+    cin.ignore();
+    fo(n){
+        getline(cin,s);
+        stringstream ss(s);
+        int word=0;
+        int number=0;
+        while (getline(ss,data,' '))
+        {
+            if(data.find("S")!=string::npos||data.find("s")!=string::npos)number++;
+            word++;
+        }
+        cout<<word<<","<<number<<endl;
+    }
+    system("Pause");
+    return 0;
+}
